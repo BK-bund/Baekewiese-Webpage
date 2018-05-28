9e7642b5b7222a9da672fc56e7d7b599455a6948
Ben Schumacher <ben.schumacher@cjf-berlin.de>
2018-05-28 09:41:20 +0200

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
 
+## 1.1.0
+- Bump kaakeli to 1.1.0
+- Fix mailer script
+
 ## 1.0.0
 - First stable version
--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function enqueue_baekewiese_kaakeli_style() {
-  wp_enqueue_style( 'baekewiese_kaakeli', get_stylesheet_directory_uri().'/style.css', array('kaakeli'), '0.1', 'all' );
+  wp_enqueue_style( 'baekewiese_kaakeli', get_stylesheet_directory_uri().'/style.css', array('kaakeli'), '1.1.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_baekewiese_kaakeli_style' );
 
