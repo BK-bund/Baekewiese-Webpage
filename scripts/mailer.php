29391f480304abe216d9bfd4473d15f5b6e587e0
Ben Schumacher <ben.schumacher@cjf-berlin.de>
2018-06-11 13:23:54 +0200

    Add option to send cc to admin & clean up code

--- scripts/mailer.php
+++ scripts/mailer.php
@@ -1,25 +1,23 @@
 <?php
 
 // ======= Konfiguration:
-
-$returnPage           = "/reservierung/success/";
-$returnErrorPage      = "/reservierung/failure/";
-$cc_mailTo            = "vermietung@baekewiese.de";
-//$cc_mailTo            = "admin@cjf-berlin.de";
-$cc_mailFrom          = "Baekewiesen Vermietungen <vermietung@baekewiese.de>";
-$cc_mailSubject       = "Neue Reservierungsanfrage: ";
-$customer_mailSubject = "Deine Reservierungsanfrage für die Bäkewiese";
-$customer_mailText    = "Vielen Dank für deine Reservierungsanfrage. Wir melde uns in Kürze bei dir.\nDeine Anfrage war:\n\n";
-$mailText = "";
-
+$returnPage             = "/reservierung/success/";
+$returnErrorPage        = "/reservierung/failure/";
+$cc_mailTo              = "vermietung@baekewiese.de";
+$admin_mailTo           = "admin@cjf-berlin.de";
+$sendCCToAdmin          = True;
+$cc_mailFrom            = "Baekewiesen Vermietungen <vermietung@baekewiese.de>";
+$cc_mailSubject         = "Neue Reservierungsanfrage: ";
+$customer_mailSubject   = "Deine Reservierungsanfrage für die Bäkewiese";
+$customer_mailText      = "Vielen Dank für deine Reservierungsanfrage für die Bäkewiese! Wir melde uns in Kürze bei dir.\n\nDeine Anfrage war:\n";
 
 $header  = "From:" .$cc_mailFrom ."\n";
-//$header .= "Reply-To: Baekewiesen Vermietungen <vermietung@baekewiese.de> \n";
 $header .= "MIME-Version: 1.0\n";
 $header .= "Content-Type: text/plain; charset=utf-8\n";
 //$header .= "Content-type: text/html;\n";// "; charset=iso-8859-1\r\n";
 
-
+// Initialisiere Variablen
+$mailText = "";
 $error = False;
 // ======= Text der Mail aus den Formularfeldern erstellen:
 
@@ -42,11 +40,11 @@ if(isset($_POST)) {
      if($name == "E-Mail_2" && $value != $customer_mailTo) {
        $error = True;
      }
+     if ($name == "E-Mail_2" || $name == "Nutzungsbedingungen") {
+       continue;
+     }
       // Wenn der Feldwert aus mehreren Werten besteht:
       // (z.B. <select multiple>
-      if ($name == "E-Mail_2") {
-        continue;
-      }
       if(is_array($value)) {
           // "Feldname:" und Zeilenumbruch dem Mailtext hinzufügen
           $mailText .= $name . ":\n";
@@ -86,10 +84,15 @@ if(isset($_POST)) {
  $mailSent1 = @mail($cc_mailTo, $cc_mailSubject, $mailText, $header);
  $mailSent2 = @mail($customer_mailTo, $customer_mailSubject, $customer_mailText . $mailText, $header);
 
+// Sende eine Kopie an den Admin
+if($sendCCToAdmin) {
+  @mail($admin_mailTo, $cc_mailSubject, $mailText, $header);
+}
+
  // ======= Return-Seite an den Browser senden
 
  // Wenn der Mailversand erfolgreich war:
- if($mailSent1 == TRUE && $mailSent2 == TRUE) {
+ if($mailSent1 && $mailSent2) {
    // Seite "Formular verarbeitet" senden:
    header("Location: " . $returnPage);
  }
